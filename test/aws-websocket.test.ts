c4bfe3875322b122291f255318955afc1a92f1b2
sasha <sasha.luhr@gmail.com>
2022-08-14 12:48:53 +0200

    init

---
+++ test/aws-websocket.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsWebsocket from '../lib/aws-websocket-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-websocket-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsWebsocket.AwsWebsocketStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
